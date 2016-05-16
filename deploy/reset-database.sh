3702aee3e3a13d0bc9c799f7ed9cc4d7facee728
Philipp Bräutigam <philipp.braeutigam@googlemail.com>
2016-05-16 21:51:26 +0200

    Updated heroku deploy hooks

---
+++ deploy/reset-database.sh
@@ -0,0 +1,2 @@
+php bin/console propel:sql:insert --force
+php bin/console jarves:install:demo demo.jarves.io / -v
---
+++ deploy/restart-dyno.sh
@@ -0,0 +1,4 @@
+curl -n -X DELETE https://api.heroku.com/apps/jarves-demo/dynos/web \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/vnd.heroku+json; version=3" \
+  -H "Authorization: Bearer $HEROKU_API_TOKEN"
